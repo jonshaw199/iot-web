fa7080db5b5f774db7be06c033fa8afbfe6b3c4f
Jon Shaw <jonshaw199@gmail.com>
2022-11-09 22:14:09 -0800

    Consuming user api

--- src/api/user.ts
+++ src/api/user.ts
@@ -10,3 +10,26 @@ export function create(user: User) {
     },
   });
 }
+
+export function update(uuid: string, user: Partial<User>) {
+  return req(`/user${uuid}`, {
+    method: "PUT",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export function deleteUser(uuid: string) {
+  return req(`/user/${uuid}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export function get() {
+  return req("/user");
+}
